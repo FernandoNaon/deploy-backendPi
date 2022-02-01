9488caf84d41db4e15b02af87147c3572d657868
Fernando Naon <74456529+FernandoNaon@users.noreply.github.com>
2022-02-01 10:41:11 -0300

    Cors config

--- src/app.js
+++ src/app.js
@@ -4,19 +4,20 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const routes = require("./routes/index.js");
 const { CORS_URL } = process.env;
-
+const cors = require("cors");
 require("./db.js");
 
 const server = express();
 
 server.name = "API";
+app.use(cors());
 
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", CORS_URL); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", CORS_URL || "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
